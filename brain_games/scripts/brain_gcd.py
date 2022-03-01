b0cb4f26cd9be4a0173107d5131732338a345e7c
Alexandr Myshkin <sir.myshkin@gmail.com>
2022-03-01 13:02:53 +0300

    Add Brain-gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+from brain_games.engine import run_game
+from brain_games.games import gcd
+
+
+def main():
+    run_game(gcd)
+
+
+if __name__ == "__main__":
+    main()
+
+
